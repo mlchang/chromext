3d78eab72732d4c9bd5d9fd8d787b78d01b561f0
mc <mc@healthnetwork.com>
2016-02-08 15:52:55 -0500

    popup html

--- popup.js
+++ popup.js
@@ -33,21 +33,43 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if(request.directive = "apiJSON"){
         var obj = request.apiResult;
-        var target = obj[Object.keys(obj)[0]];
-        var categories = obj[Object.keys(obj)[0]];
+        var ratings = obj[Object.keys(obj)[0]];
+        var categories = obj[Object.keys(obj)[1]];
         
 
-        document.getElementById('zeroRep').innerHTML = target["0"][0];
-        document.getElementById('zeroCon').innerHTML = target["0"][1];
-        document.getElementById('oneRep').innerHTML  = target["1"][0];
-        document.getElementById('oneCon').innerHTML  = target["1"][1];
-        document.getElementById('twoRep').innerHTML  = target["2"][0];
-        document.getElementById('twoCon').innerHTML  = target["2"][1];
-        document.getElementById('fourRep').innerHTML = target["4"][0];
-        document.getElementById('fourCon').innerHTML = target["4"][1];
+        document.getElementById('zeroRep').innerHTML = ratings["0"][0];
+        document.getElementById('zeroCon').innerHTML = ratings["0"][1];
+        document.getElementById('oneRep').innerHTML  = ratings["1"][0];
+        document.getElementById('oneCon').innerHTML  = ratings["1"][1];
+        document.getElementById('twoRep').innerHTML  = ratings["2"][0];
+        document.getElementById('twoCon').innerHTML  = ratings["2"][1];
+        document.getElementById('fourRep').innerHTML = ratings["4"][0];
+        document.getElementById('fourCon').innerHTML = ratings["4"][1];
 
-        var jsonStr = JSON.stringify(request.apiResult);
-        document.getElementById('jsonResult').innerHTML = jsonStr;
+        document.getElementById('target').innerHTML = ratings["target"];
+        //document.getElementById('categories').innerHTML = categories;
+        var categoryString = JSON.stringify(ratings["categories"]);
+        document.getElementById('categories').innerHTML = categoryString;
+        var safe;
+        var list = "";
+        for(var i = 0; i < 4; i++){
+          if(i == 3){
+            i++;
+          }
+          list == list +  ", ";
+          list == list +  i.toString();
+          if( ratings[i.toString()][0] > 80){
+            safe = false;
+
+          }
+        }
+
+        document.getElementById('list').innerHTML = list;
+        if(safe == false){
+          document.getElementById('safetyRating').innerHTML = 'This site has excellent safety';
+        } else {
+          document.getElementById('safetyRating').innerHTML = 'This site does not have excellent safety';
+        }
       }
     }
 );
